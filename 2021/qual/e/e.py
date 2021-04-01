8c042426571e8b68f4341d4168c43eeec347d4ac
Miguel Diaz <diazvillarroelm@gmail.com>
2021-04-01 18:07:43 +0000

    Add solution to Qualification 2021 - Problem E - Cheating Detection

--- 2021/qual/e/e.py
+++ 2021/qual/e/e.py
@@ -1,67 +1,38 @@
 from collections import Counter
-from itertools import groupby
 from operator import itemgetter
-import statistics
 import random
-import math
-
-def cheater_likelyhood(SR, err):
-    Rs = []
-    for block_size in [50, 100, 500, 1000, 2000]:
-        C = Counter(SR[-block_size:])
-        print (C)
-        Rs.append(block_size - abs(C['0'] - C['1']))
-
-    Rs.append(sum(x == '1' for x in SR))
-    Rs.append(sum(x == '0' for x in SR[-100:]))
-    Rs.append(err)
-    weights = [250, 100, 10, 10, 5, 1, -20, ]
-
-    Rs.insert(0, sum (r*w for r, w in zip(Rs, weights)))
-    return tuple(Rs)
-
-def remap(base, step, Fn):
-    return [base + (step*v) for v in Fn]
-
-def f(p, q):
-    x = p - q
-    return 1 / (1 + math.exp(-x))
-
-def error(actual, expected):
-    return sum(a != e for a, e in zip(actual, expected))
 
 def cheater(Rs):
-    Qs = [0] * 10000
-    ok = []
-    for r in Rs:
-        k = 0
-        for i, v in enumerate(r):
-            Qs[i] += v == '0'
-            k += v == '1'
-        ok.append(k)
+    QD = [0] * 10000 # Question Difficulty
+    PS = [0] * 100   # Player Score
+    for i, R in enumerate(Rs):
+        for j, r in enumerate(R):
+            QD[j] += r == '0' # Most mistakes ==> harder question
+            PS[i] += r == '1' # Correct answer
 
-    # Remap by skill
-    PQs = [q/100 for q in Qs]
-    PQRemap = remap(-3.0, 0.06, Qs)
-    QDs = sorted([(q, i) for i, q in enumerate(PQRemap)])
+    # Permutations
+    PP = sorted(enumerate(PS), key=itemgetter(1))
+    QP = sorted(enumerate(QD), key=itemgetter(1))
 
-    PRemap = remap(-3, 0.0006, ok)
-    PDs = sorted([(p, i) for i, p in enumerate(PRemap)])
+    M = []  # Sorted matrix
+    TD = [] # Tail delta
+    for p, _ in PP:
+        M.append(''.join(Rs[p][q] for q, _ in QP[-500:]))
+        C = Counter(M[-1])
+        TD.append(C['1'] - C['0'])
 
-    # appearance order
-    SPs = [p for _, p in sorted([(i, p) for p, i in PDs])]
-    SQs = [q for _, q in sorted([(i, q) for q, i in QDs])]
+    # Compare average of similarly ranked players on the hardest questions
+    sus = []
+    BLOCK = 5
+    THRESHOLD = 100
+    while not sus:
+        for k in range(101-BLOCK):
+            avg = sum(TD[k+i] for i in range(BLOCK)) / BLOCK
+            sus += [PP[k+i][0] for i in range(BLOCK) if TD[k+i]-avg > THRESHOLD]
 
-    err = [0] * 100
-    sig = [0] * 10000
-    for p in range(100):
-        for q in range(10000):
-            sig[q] = '1' if random.random() <= f(SPs[p], SQs[q]) else '0'
-        err[p] = error(Rs[p], sig)
+        THRESHOLD -= 5
 
-    Es = sorted((e, i) for i, e in enumerate(err))
-    E = [e for _, e in sorted([(i, e) for e, i in Es])]
-    return Es[-1][1]+1
+    return random.choice(sus)+1
 
 T = int(input())
 P = int(input())
